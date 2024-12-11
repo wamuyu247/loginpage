c32dd0ac6a25a3401184cdc144c913c29bb1ceb4
wamuyu247 <faith.wamuyu20@students.dkut.ac.ke>
2024-12-11 07:59:45 +0300

    commit

---
+++ app/src/main/java/com/example/loginsignup/Register.java
@@ -0,0 +1,13 @@
+package com.example.loginsignup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
